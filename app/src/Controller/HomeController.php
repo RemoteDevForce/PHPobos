708dd8505f037420a24fa9c9d223646871647e2f
Joshua Copeland <Josh@RemoteDevForce.com>
2021-09-10 10:27:43 -0700

    routes

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/on', name: 'on')]
+    public function on(): Response
+    {
+        return $this->render('home/on.html.twig');
+    }
+}
